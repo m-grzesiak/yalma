3c01304f3c15ef1034ab7e52094d3a265f76b788
Michal Grzesiak <10697444+m-grzesiak@users.noreply.github.com>
2020-11-13 14:53:06 +0100

    Remove config.ini file from the project source and add a readme file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # yalma
+
+## Usage
+
+1. Install required dependencies by `pip install -r requirements.txt`
+2. Run the application by `python yalma.py` ;)
+
+Before you run the app, please create a `config.ini` file at `$HOME/.config/yalma/` path:
+
+```ini
+[luxmed]
+username = 
+password = 
+language = en
+
+[email_settings]
+username = 
+password = 
+smtp_server = 
+smtp_port = 
+```
+
+### Luxmed section
+* you have to provide your `username` and `password` there;
+* `language` position has 2 possible values: `en` or `pl` - this setting has not any influence on returned data 
+(like specialization names) from the Luxmed system - it only defines the language of errors you can receive from that
+ API. 
+ 
+ ### Email settings section
+ These are settings for sending email to you when any visits are available.
+ * `username` and `password` is pretty obvious ;)
+ * SMTP server and port depend on your mail server. For instance, for Gmail it will be `smtp.gmail.com` and `465` 
+ as a port.
--- config_loader.py
+++ config_loader.py
@@ -1,5 +1,7 @@
 import codecs
+import platform
 from configparser import ConfigParser
+from pathlib import Path
 
 _CONFIGURATION_FILE_NAME = 'config.ini'
 
@@ -8,15 +10,35 @@ class ConfigurationValidationException(Exception):
     pass
 
 
-def _prepare_validation_error_message(configuration_section, field):
+def _get_configuration_directory_path() -> str:
+    current_platform = platform.system()
+    user_home_path = Path.home()
+
+    if current_platform == 'Linux' or current_platform == 'Darwin':
+        return str(user_home_path) + '/.config/yalma/'
+    else:
+        if current_platform == 'Windows':
+            return str(user_home_path) + '/AppData/Local/yalma/'
+
+
+def _prepare_validation_error_message(configuration_section, field) -> str:
     return "A value for field '{}' in section '{}' is not set in a {} file".format(field,
                                                                                    configuration_section,
                                                                                    _CONFIGURATION_FILE_NAME)
 
 
+def initialize_app_configuration():
+    configuration_directory_path = _get_configuration_directory_path()
+    Path(configuration_directory_path).mkdir(parents=False, exist_ok=True)
+    path_to_config_file = configuration_directory_path + _CONFIGURATION_FILE_NAME
+    Path(path_to_config_file).touch()
+
+
 def read_configuration(configuration_section: str, expected_mandatory_fields: [] = ()) -> {}:
+    path_to_config_file = _get_configuration_directory_path() + _CONFIGURATION_FILE_NAME
     config_parser = ConfigParser()
-    with codecs.open(_CONFIGURATION_FILE_NAME, 'r', encoding='utf-8') as config_file:
+
+    with codecs.open(path_to_config_file, 'r', encoding='utf-8') as config_file:
         config_parser.read_file(config_file)
         configuration = dict(config_parser.items(configuration_section))
 
