f3b7a960667245d36228206ab7cc1c56edeef81a
Michal Grzesiak <10697444+m-grzesiak@users.noreply.github.com>
2021-02-21 14:46:34 +0100

    Do not display results in a table view when there are no results to show

--- yalma.py
+++ yalma.py
@@ -8,8 +8,12 @@ import monitoring
 from luxmed_api import LuxmedApi
 
 
-def _prepare_table_view(data, headers):
-    return tabulate(data, headers=headers, tablefmt="psql")
+def _display_results(data, headers):
+    if data:
+        table_view = tabulate(data, headers=headers, tablefmt="psql")
+        print(table_view)
+    else:
+        print("No results have found for given criteria")
 
 
 @click.group()
@@ -20,18 +24,16 @@ def main():
 @main.command(help='get a list of available cities')
 def cities():
     api = LuxmedApi()
-    cities = api.get_cities()
-    table_view = _prepare_table_view(cities, ["city ID", "city name"])
-    print(table_view)
+    retrieved_cities = api.get_cities()
+    _display_results(retrieved_cities, ["city ID", "city name"])
 
 
 @main.command(help='get a list of available clinics')
 @click.option('-c', '--city-id', type=int, required=True, help='return a list of clinics for the given city ID')
 def clinics(city_id):
     api = LuxmedApi()
-    clinics = api.get_clinics(city_id)
-    table_view = _prepare_table_view(clinics, ["clinic ID", "clinic name"])
-    print(table_view)
+    retrieved_clinics = api.get_clinics(city_id)
+    _display_results(retrieved_clinics, ["clinic ID", "clinic name"])
 
 
 @main.command(help='get a list of available doctors')
@@ -41,17 +43,15 @@ def clinics(city_id):
 def doctors(city_id, service_id, clinic_id):
     api = LuxmedApi()
     retrieved_doctors = api.get_doctors(city_id, service_id, clinic_id)
-    table_view = _prepare_table_view(retrieved_doctors, ["doctor ID", "doctor name"])
-    print(table_view)
+    _display_results(retrieved_doctors, ["doctor ID", "doctor name"])
 
 
 @main.command(help='get a list of available services')
 @click.option('-c', '--city-id', type=int, required=True, help='return a list of services for the given city ID')
 def services(city_id):
     api = LuxmedApi()
-    services = api.get_services(city_id)
-    table_view = _prepare_table_view(services, ["service ID", "service name"])
-    print(table_view)
+    retrieved_services = api.get_services(city_id)
+    _display_results(retrieved_services, ["service ID", "service name"])
 
 
 @main.command(help='monitor the availability of visits for the given criteria')
