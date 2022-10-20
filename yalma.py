264abbd94e7444a4fb922e27f5740325bc367960
Michał Grzesiak <10697444+m-grzesiak@users.noreply.github.com>
2022-10-20 19:54:08 +0200

    refactor: clean up code, extract non-API code to separate service

--- yalma.py
+++ yalma.py
@@ -3,17 +3,9 @@ from datetime import date
 import click
 from tabulate import tabulate
 
+import booking_service
 import config_loader
-import luxmed_api
-import report_generator
-
-
-def __display_results(data, headers):
-    if data:
-        table_view = tabulate(data, headers=headers, tablefmt="psql")
-        print(table_view)
-    else:
-        print("No results have found for given criteria")
+import report_service
 
 
 @click.group()
@@ -23,7 +15,7 @@ def main():
 
 @main.command(help='get a list of available cities')
 def cities():
-    retrieved_cities = luxmed_api.get_cities()
+    retrieved_cities = booking_service.get_cities()
     __display_results(retrieved_cities, ["city ID", "city name"])
 
 
@@ -31,7 +23,7 @@ def cities():
 @click.option('-c', '--city-id', type=int, required=True, help='return a list of clinics for the given city ID')
 @click.option('-s', '--service-id', type=int, required=True, help='a service that doctors should be specialized in')
 def clinics(city_id, service_id):
-    retrieved_clinics = luxmed_api.get_clinics(city_id, service_id)
+    retrieved_clinics = booking_service.get_clinics(city_id, service_id)
     __display_results(retrieved_clinics, ["clinic ID", "clinic name"])
 
 
@@ -40,13 +32,13 @@ def clinics(city_id, service_id):
 @click.option('-s', '--service-id', type=int, required=True, help='a service that doctors should be specialized in')
 @click.option('-cl', '--clinic-id', type=int, help='a clinic where you are looking for doctors')
 def doctors(city_id, service_id, clinic_id):
-    retrieved_doctors = luxmed_api.get_doctors(city_id, service_id, clinic_id)
+    retrieved_doctors = booking_service.get_doctors(city_id, service_id, clinic_id)
     __display_results(retrieved_doctors, ["doctor ID", "doctor name"])
 
 
 @main.command(help='get a list of available services')
 def services():
-    retrieved_services = luxmed_api.get_services()
+    retrieved_services = booking_service.get_services()
     __display_results(retrieved_services, ["service ID", "service name"])
 
 
@@ -73,8 +65,18 @@ def services():
 def monitor(email, city_id, service_id, from_date, to_date, time_of_day, clinic_id=None, doctor_id=None):
     parsed_from_date = from_date.date()
     parsed_to_date = to_date.date()
-    report_generator.make_report(email, city_id, service_id, parsed_from_date, parsed_to_date, time_of_day, clinic_id,
-                                 doctor_id)
+    available_terms = booking_service.get_available_terms(city_id, service_id, parsed_from_date, parsed_to_date,
+                                                          time_of_day, clinic_id, doctor_id)
+    report_service.make_report(available_terms, email)
+
+
+def __display_results(results: {}, headers: [str]):
+    if results:
+        parsed_results = [(item['id'], item['name']) for item in results]
+        table_view = tabulate(parsed_results, headers=headers, tablefmt="psql")
+        print(table_view)
+    else:
+        print("No results have found for given criteria")
 
 
 if __name__ == '__main__':
